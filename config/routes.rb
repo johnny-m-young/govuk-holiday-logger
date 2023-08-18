d11f4491109e97ddc1177785f7f098c5675e5686
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-08-18 12:56:08 +0100

    Generate a User model with devise
    This commit runs `rails g devise User role:string` to
    generate a User model with a role.
    
    I added a default for User role to be "user", this will
    be the lowest level of access for the application, thus
    making use of the deny by default principle.
    I also altered the User table to include `first_name` and
    `last_name` columns as I anticipate we will need this
    later for personalised dashboard messages.
    
    Finally I ran a `rails db:migrate`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 end

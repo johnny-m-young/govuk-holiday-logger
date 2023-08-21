300f30720b89fa18a00f361992e98fdfd07819d6
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-08-21 13:33:40 +0100

    Add validations to the User model
    This validates that a User has the necessary
    attributes on creation. It asserts the email
    address follows the format of  a GOV.UK employee
    email address and that the User has a first name
    and last name (which will be needed for
    personalised rendering of the homepage)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@digital.cabinet-office.gov.uk+\z/ }
+  validates :given_name, presence: true
+  validates :family_name, presence: true
+  validates :password, length: { minimum: 8, message: "must be at least 8 characters long" }
 end

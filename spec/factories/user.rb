856fe614905ab3bca81bc8ed36547acddefb367f
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-08-25 12:50:58 +0100

    Update the user factory
    Adds the new annual_leave_remaining
    attribute to the User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     family_name { "Smith" }
     email { "john.smith@digital.cabinet-office.gov.uk" }
     password { "password with $ymb0l$" }
+    annual_leave_remaining { 27.5 }
   end
 end

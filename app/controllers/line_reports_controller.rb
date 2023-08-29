28ebe5617026e02997d8489f38aa787d7a67450f
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-08-29 10:56:27 +0100

    Add LineReports resource
    This adds a resource for LineReports, limited to the
    show action. A model is not necessary here because
    line reports use the User model.

---
+++ app/controllers/line_reports_controller.rb
@@ -0,0 +1,13 @@
+class LineReportsController < ApplicationController
+  before_action :redirect_if_user_not_line_manager
+
+  def show
+    @line_reports = current_user.line_reports
+  end
+
+private
+
+  def redirect_if_user_not_line_manager
+    redirect_to root_path unless current_user.is_line_manager?
+  end
+end

08ffcd1c371e944909e3cb82c3df49503ad3aa23
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-08-31 15:12:45 +0100

    Refactor User model tests
    Makes use of the User factory to DRY
    out code.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,44 +2,24 @@ require "spec_helper"
 
 RSpec.describe User, type: :model do
   it "accepts valid email address formats" do
-    valid_email = described_class.new(
-      email: "right_format@digital.cabinet-office.gov.uk",
-      given_name: "John",
-      family_name: "Smith",
-      password: "password",
-    )
+    valid_email = build(:user)
     expect(valid_email.valid?).to be true
   end
 
   it "rejects email inputs that are not emails" do
-    invalid_email = described_class.new(
-      email: "wrong_format",
-      given_name: "John",
-      family_name: "Smith",
-      password: "password",
-    )
+    invalid_email = build(:user, email: "wrong_format")
     expect(invalid_email.valid?).to be false
     expect(invalid_email.errors.first.full_message).to eq("Email is invalid")
   end
 
   it "rejects unsupported email domains" do
-    invalid_domain = described_class.new(
-      email: "invalid_domain@gmail.com",
-      given_name: "John",
-      family_name: "Smith",
-      password: "password",
-    )
+    invalid_domain = build(:user, email: "invalid_domain@gmail.com")
     expect(invalid_domain.valid?).to be false
     expect(invalid_domain.errors.first.full_message).to eq("Email is invalid")
   end
 
   it "rejects passwords shorter than 8 characters" do
-    short_password = described_class.new(
-      email: "valid_email@digital.cabinet-office.gov.uk",
-      given_name: "John",
-      family_name: "Smith",
-      password: "1234567",
-    )
+    short_password = build(:user, password: "1234567")
     expect(short_password.valid?).to be false
     expect(short_password.errors.first.full_message).to eq("Password must be at least 8 characters long")
   end

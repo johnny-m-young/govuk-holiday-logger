2f9e1abf811737502ad55c82bce182307295c2b3
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-09-01 14:44:22 +0100

    Refactor RootController
    Makes use of the before_action method
    in rails to avoid putting conditionals
    in the index method.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,12 @@
 class RootController < ApplicationController
+  before_action :redirect_if_user_not_signed_in
+
   def index
+  end
+
+private
+
+  def redirect_if_user_not_signed_in
     redirect_to new_user_session_path unless current_user
   end
 end

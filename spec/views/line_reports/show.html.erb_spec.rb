d5dae5f39b7e95b612c7460aaaf06313610261ab
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-09-04 12:16:06 +0100

    Add view tests for line_reports#show
    These tests make sure the status is updated as
    required on the line_reports#show.

---
+++ spec/views/line_reports/show.html.erb_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+RSpec.describe "line_reports/show", type: :view do
+  let(:line_manager) { create(:user) }
+  let(:line_report) { create(:user, line_manager_id: line_manager.id) }
+
+  setup do
+    sign_in line_manager
+  end
+
+  context "when a line report has pending annual leave requests" do
+    it "the line report's status is 'Pending requests'" do
+      create(:annual_leave_request, user_id: line_report.id, status: "pending")
+      assign(:line_reports, line_manager.line_reports)
+
+      render
+
+      expect(rendered).to have_content("Pending requests")
+      expect(rendered).to have_css(".govuk-tag--red")
+    end
+  end
+
+  context "when a line report has no pending annual leave requests" do
+    it "the line report's status is 'No pending requests'" do
+      create(:annual_leave_request, user_id: line_report.id, status: "approved")
+      assign(:line_reports, line_manager.line_reports)
+
+      render
+
+      expect(rendered).to have_content("No pending requests")
+      expect(rendered).to have_css(".govuk-tag--green")
+    end
+  end
+end

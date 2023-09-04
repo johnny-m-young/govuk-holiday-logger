48cb8ea98fe4098ce5b83faa18e98b934f6b8208
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-09-04 15:36:26 +0100

    Fix annual_leave_request resource
    Routes was previously using the `resource:` syntax
    which doesn't support automatic indexing of database
    entries (e.g. annual_leave_requests/1/edit).
    
    This commit updates the routes to make use of the
    `resources:` syntax which does support automatic
    indexing of database entries. It also updates path
    names to their new values as these changed as result
    of the update (e.g. new_annual_leave_requests_path
    => new_annual_leave_request_path).
    
    This change will make it easier to complete upcoming
    work relating to editing and withdrawing annual leave
    requests via the dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 
   resource :line_reports, only: [:show]
-  resource :annual_leave_requests, only: %i[new create]
-  get "/annual_leave_requests/check", to: "annual_leave_requests#check"
-  get "/annual_leave_requests/confirmation", to: "annual_leave_requests#confirm"
+  resources :annual_leave_requests, only: %i[new create]
+  get "/annual_leave_requests/check", to: "annual_leave_requests#check", as: "check_annual_leave_request"
+  get "/annual_leave_requests/confirmation", to: "annual_leave_requests#confirm", as: "annual_leave_request_confirmation"
 
   mount GovukPublishingComponents::Engine, at: "/component-guide" if Rails.env.development?
 end

c4f45ca7567ba2c7d90fd0e0eac6965bcdb9edc4
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-09-06 17:04:56 +0100

    Implement approval email sending
    Implements code to send an email to a user
    when their annual leave request has been
    approved.

--- spec/helpers/emails_helper_spec.rb
+++ spec/helpers/emails_helper_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe EmailsHelper, type: :helper do
       expect(response.subject).to eq("GOV.UK Holiday Logger – #{user_full_name} – New Annual Leave Request")
     end
   end
+
+  describe "#send_approved_request_email" do
+    it "sends an approval email to the user" do
+      @client = Notifications::Client.new(ENV["NOTIFY_TEST_API_KEY"])
+
+      response_notification = helper.send_approved_request_email(annual_leave_request)
+      response = client.get_notification(response_notification.id)
+
+      expect(response.email_address).to eq(user.email)
+      expect(response.subject).to eq("GOV.UK Holiday Logger – Annual Leave Request Approved")
+    end
+  end
 end

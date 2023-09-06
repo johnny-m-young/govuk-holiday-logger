f2cbf4c567918f3a2e063ad927242edd05f54040
Jonathan Young <jonathan.young@digital.cabinet-office.gov.uk>
2023-09-06 10:42:22 +0100

    Add line_reports#show endpoint
    This action allows a manager to view a line
    report's current annual leave requests. It
    will also be the place at which the line manager
    can approve or deny each request.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 
-  resources :line_reports, only: [:index]
-  resources :annual_leave_requests, only: %i[new create]
+  resources :line_reports, only: %i[index show]
   get "/annual_leave_requests/check", to: "annual_leave_requests#check", as: "check_annual_leave_request"
   get "/annual_leave_requests/confirmation", to: "annual_leave_requests#confirm", as: "annual_leave_request_confirmation"
 
